3aba00ae7279d15b77efed187dc09b9ac2e12573
vdcsolutions <vdc.solutions@gmail.com>
2023-04-21 08:03:07 +0200

    initial

--- main.py
+++ main.py
@@ -1,17 +1,37 @@
-from fastapi import FastAPI, Body
-from typing import List, Dict
-from scraper.scraper import Scraper
-from pydantic import BaseModel
+from proxy_list import get_proxies, get_random_proxy
+from fastapi import FastAPI, Request
+import aiohttp
+import random
+import configparser
+import logging
+import uvicorn
+
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+timestamp, proxy_list = get_proxies()
+print(proxy_list)
 
 app = FastAPI()
+@app.post("/fetch")
+async def fetch_urls(request: Request):
+    target_urls = await request.json()
+    responses = []
 
-class ScrapeRequest(BaseModel):
-    urls: List[str]
-    selectors: List[str]
-    names: List[str]
+    async with aiohttp.ClientSession() as session:
+        for url in target_urls['target_urls']:
+            try:
+                proxy = get_random_proxy(proxy_list)
+                async with session.get(url, proxy=proxy, timeout=10) as response:
+                    response_text = await response.text()
+                    responses.append({'url': url, 'response': response_text})
+            except Exception as e:
+                logging.debug(f"Error fetching URL {url}: {e}")
+                responses.append({'url': url, 'response': f"Error fetching URL {url}: {e}"})
 
-@app.post("/scrape")
-async def scrape(data_to_scrape: ScrapeRequest):
-    scraper = Scraper(data_to_scrape.dict())
-    results = await scraper.scrape()
-    return results
+    return responses
+
+
+
+
