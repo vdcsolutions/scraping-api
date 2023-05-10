ca0eeaf0849a15a5b89e3265ef3594c86e382192
vdcsolutions <86836806+vdcsolutions@users.noreply.github.com>
2023-05-11 00:05:15 +0200

    Update proxy_list.py

--- proxy_list.py
+++ proxy_list.py
@@ -2,18 +2,22 @@ import requests
 import configparser
 import logging
 import random
+from typing import List
+
 
 logging.basicConfig(filename='get_proxy_list.log', level=logging.DEBUG)
 
-# Log a message
-logging.debug('This is a debug message')
 
-config = configparser.ConfigParser()
+def get_from_api(api_url: str) -> dict:
+    """
+    Fetches data from the specified API URL.
 
-config.read('config.ini')
+    Args:
+        api_url (str): The URL of the API to fetch data from.
 
-
-def get_from_api(api_url):
+    Returns:
+        dict: JSON response from the API.
+    """
     # Send a GET request to the API
     response = requests.get(api_url)
 
@@ -27,14 +31,36 @@ def get_from_api(api_url):
 
     return data
 
-def get_timestamp(data):
+
+def get_timestamp(data: List[dict]) -> str:
+    """
+    Retrieves the timestamp from the provided data.
+
+    Args:
+        data (List[dict]): List of dictionaries containing data.
+
+    Returns:
+        str: The timestamp value.
+    """
     try:
         timestamp = data[0]['timestamp']
-    except:
-        logging.debug('There is no timestamp in downloaded json')
+    except IndexError:
+        logging.debug('There is no timestamp in the downloaded JSON')
+        timestamp = ''
+
     return timestamp
 
-def get_proxies_from_json(data):
+
+def get_proxies_from_json(data: List[dict]) -> List[str]:
+    """
+    Extracts proxy URLs from the provided data.
+
+    Args:
+        data (List[dict]): List of dictionaries containing proxy data.
+
+    Returns:
+        List[str]: List of proxy URLs.
+    """
     urls = []
     for d in data:
         url = f"http://{d['ip']}:{d['port']}"
@@ -43,17 +69,37 @@ def get_proxies_from_json(data):
     return urls
 
 
-def get_proxies():
-    data = get_from_api(config.get('PRODUCTION','proxy_api_url'))
+def get_proxies() -> tuple:
+    """
+    Retrieves the timestamp and proxy URLs from the API.
+
+    Returns:
+        tuple: A tuple containing the timestamp and a list of proxy URLs.
+    """
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+
+    data = get_from_api(config.get('PRODUCTION', 'proxy_api_url'))
     timestamp = get_timestamp(data)
     urls = get_proxies_from_json(data)
     return timestamp, urls
 
-def get_random_proxy(proxy_list):
+
+def get_random_proxy(proxy_list: List[str]) -> str:
+    """
+    Returns a random proxy URL from the provided list.
+
+    Args:
+        proxy_list (List[str]): List of proxy URLs.
+
+    Returns:
+        str: A randomly selected proxy URL.
+    """
     proxy = random.choice(proxy_list)
     return proxy
 
+
 if __name__ == '__main__':
-    timestamp, urls = get_data()
+    timestamp, urls = get_proxies()
     print(timestamp)
     print(urls)
